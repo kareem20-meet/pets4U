63f7d34850bf51032f5dd3912138805c48bde246
omersg20-meet <omersg20@meet.mit.edu>
2019-03-31 09:49:48 -0400

    progress

---
+++ PetDetail.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+
+
+    let AvilablePets = [$("#simon"), $("#steve"), $("#katty")];
+
+    function CheckPicked(number) {
+
+
+    }
+
+});
--- home.js
+++ home.js
@@ -1,12 +1,5 @@
 $(document).ready(function(){
 
 
-    const c1=$('#dogCard');
-
-    c1.click(function() {
-        alert("hi");
-
-    });
-
 
 });
