9be058b061874418dd0f681b3c9e754141df0ade
omersg20-meet <omersg20@meet.mit.edu>
2019-03-31 17:18:16 -0400

    progress

--- PetDetail.js
+++ PetDetail.js
@@ -1,11 +1,74 @@
 $(document).ready(function(){
 
 
-    let AvilablePets = [$("#simon"), $("#steve"), $("#katty")];
+    let AvilablePets = [$("#steve"), $("#simon"), $("#katty")];
 
+
+ let Pet1=AvilablePets[0];
+ let Pet2=AvilablePets[1];
+ let Pet3=AvilablePets[2];
+let Clicked=false;
+
+  Pet1.click(function () {
+      CheckPicked(AvilablePets.indexOf(Pet1));
+  });
+
+    Pet2.click(function () {
+        CheckPicked(AvilablePets.indexOf(Pet2));
+    });
+
+
+    Pet3.click(function () {
+        CheckPicked(AvilablePets.indexOf(Pet3));
+    });
+
+
+    function HideAndShow() {
+        if(Clicked===false){
+            $("#PetId").hide();
+
+        }
+        else {
+            $("#PetId").show();
+        }
+    }
     function CheckPicked(number) {
+        Clicked = true;
+        HideAndShow();
+        if (number == 0) {
+            $("#name").html("<h3>Name: Steve</h3>");
+            $("#age").html("<h3>Age:2years</h3>");
+            $("#food").html("<h3>Favorite food : Caviar</h3>");
 
+            $("#Adress").html("<h3>Adress:dest st kfar qara</h3>");
+        }
+
+        if (number == 1) {
+
+                $("#name").html("<h3>Name: Simon</h3>");
+                $("#age").html("<h3>Age:2years</h3>");
+                $("#food").html("<h3>Favorite food : Fish </h3>");
+
+                $("#Adress").html("<h3>Adress:tel Aviv</h3>");
+
+
+        }
+
+
+        if (number == 2) {
+
+            $("#name").html("<h3>Name: Katty</h3>");
+            $("#age").html("<h3>Age:1years</h3>");
+            $("#food").html("<h3>Favorite food : Meat Balls </h3>");
+
+            $("#Adress").html("<h3>Adress:Nazareth</h3>");
+
+
+        }
 
     }
+
+
+$("#PetId").hide();
 
 });
